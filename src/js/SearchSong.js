639f5e46d958fe595d05fadac9d54099fcb5a73f
HeroutRozilyo <heroot12@gmail.com>
2023-07-26 22:19:03 +0300

    search

--- src/js/SearchSong.js
+++ src/js/SearchSong.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import '../css/SearchSong.css';
 
 function SearchSongs() {
@@ -6,47 +6,59 @@ function SearchSongs() {
   const [searchResults, setSearchResults] = useState([]);
   const [isSearching, setIsSearching] = useState(false);
   const [isInputFocused, setIsInputFocused] = useState(false);
-  const CACHE_KEY = 'songCache';
+  const searchRef = useRef();
 
   useEffect(() => {
-    // Fetch songs from the server and save to cache
-    const fetchSongsAndCache = async () => {
-      try {
-        const response = await fetch(`http://localhost:3001/api/songs`);
-        const data = await response.json();
-        const cacheData = {
-          timestamp: Date.now(),
-          data: data,
-        };
-        localStorage.setItem(CACHE_KEY, JSON.stringify(cacheData));
-      } catch (error) {
-        console.error(error);
+    const delayDebounceFn = setTimeout(() => {
+      if (searchTerm.trim() !== '' || isInputFocused) {
+        fetchSearchResults();
+      } else {
+        setSearchResults([]);
+      }
+    }, 500);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [searchTerm, isInputFocused]);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (searchRef.current && !searchRef.current.contains(event.target)) {
+        setSearchResults([]);
+        setIsInputFocused(false);
       }
     };
 
-    fetchSongsAndCache();
-    const interval = setInterval(fetchSongsAndCache, 10 * 60 * 1000); // 10 minutes interval
-    return () => clearInterval(interval);
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
   }, []);
 
-  useEffect(() => {
-    handleSearch();
-  }, [searchTerm]);
+  const fetchSearchResults = async () => {
+    try {
+      setIsSearching(true);
 
-  const handleSearch = () => {
-    // Perform search on the cached data
-    const cachedData = JSON.parse(localStorage.getItem(CACHE_KEY));
-    if (cachedData && cachedData.data) {
-      const filteredResults = cachedData.data.filter((song) =>
-        song.SongName.toLowerCase().includes(searchTerm.toLowerCase())
-      );
-      setSearchResults(filteredResults.slice(0, 10)); // Limit the number of results to 10 for performance
+      const response = await fetch(`http://localhost:3001/api/songs?search=${searchTerm}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      const data = await response.json();
+      setSearchResults(data.slice(0, 10)); // Limit the number of results to 10 for performance
+    } catch (error) {
+      console.error(error);
+      alert('An error occurred while fetching songs');
+    } finally {
+      setIsSearching(false);
     }
   };
 
   return (
+    
     <div className="search-songs">
-      <div className="search-input-container">
+      <div className="search-input-container" ref={searchRef}>
         <input
           type="text"
           value={searchTerm}
@@ -55,7 +67,7 @@ function SearchSongs() {
           onFocus={() => setIsInputFocused(true)}
           onBlur={() => setIsInputFocused(false)}
         />
-        <button className="search-button" onClick={handleSearch}>
+        <button className="search-button" onClick={fetchSearchResults}>
           Search
         </button>
       </div>
@@ -63,15 +75,14 @@ function SearchSongs() {
         {isSearching ? (
           <p>Loading...</p>
         ) : (
-          Array.isArray(searchResults) ? (
-            <ul>
-              {searchResults.map((song) => (
-                <li key={song.id}>{song.SongName}</li>
-              ))}
-            </ul>
-          ) : (
-            <p>No search results found.</p>
-          )
+          <ul>
+            {searchResults.map((song) => (
+              <li key={song.id}>{song.SongName}</li>
+            ))}
+          </ul>
+        )}
+        {searchResults.length === 0 && !isSearching && isInputFocused && (
+          <p className="no-results">No results found.</p>
         )}
       </div>
     </div>
