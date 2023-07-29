fa8a43e41f5d79ebce15bcaeedb32bd10db7f5f9
HeroutRozilyo <heroot12@gmail.com>
2023-07-29 23:50:47 +0300

    search

--- src/js/SearchSong.js
+++ src/js/SearchSong.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import '../css/SearchSong.css';
 import { Link, useNavigate } from "react-router-dom";
+
 function SearchSongs() {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
   const [isSearching, setIsSearching] = useState(false);
   const [isInputFocused, setIsInputFocused] = useState(false);
+  const [isMouseOverResults, setIsMouseOverResults] = useState(false); // New state variable
   const CACHE_KEY = 'songCache';
   const history = useNavigate();
+
   useEffect(() => {
     // Check if the cache exists and is not empty
     const cachedData = JSON.parse(localStorage.getItem(CACHE_KEY));
@@ -26,13 +29,12 @@ function SearchSongs() {
           console.error(error);
         }
       };
-  
+
       fetchSongsAndCache();
       const interval = setInterval(fetchSongsAndCache, 10 * 60 * 1000); // 10 minutes interval
       return () => clearInterval(interval);
     }
   }, []);
-  
 
   useEffect(() => {
     handleSearch();
@@ -48,12 +50,18 @@ function SearchSongs() {
       setSearchResults(filteredResults); // Limit the number of results to 10 for performance
     }
   };
+
   const handleSongSelect = (songCode) => {
     history(`/song/${songCode}`);
   };
+
   return (
     <div className="search-songs">
-      <div className="search-input-container">
+      <div
+        className="search-input-container"
+        onMouseEnter={() => setIsMouseOverResults(true)} // Handle mouse over the search results
+        onMouseLeave={() => setIsMouseOverResults(false)} // Handle mouse leave the search results
+      >
         <input
           type="text"
           value={searchTerm}
@@ -66,24 +74,26 @@ function SearchSongs() {
           Search
         </button>
       </div>
-      <div className="search-results">
-        {isSearching ? (
-          <p>Loading...</p>
-        ) : (
-          Array.isArray(searchResults) ? (
+      {isInputFocused || isMouseOverResults ? ( // Only show the search results when either the input is focused or the mouse is over the results
+        <div className="search-results">
+          {isSearching ? (
+            <p>Loading...</p>
+          ) : Array.isArray(searchResults) && searchResults.length > 0 ? (
             <ul>
               {searchResults.map((song) => (
-               <li key={song.id} onMouseDown={(e) => handleSongSelect(song.videoId, e)}>
-               {song.SongName}
-             </li>
-
+                <li
+                  key={song.id}
+                  onMouseDown={(e) => handleSongSelect(song.videoId, e)}
+                >
+                  {song.SongName}
+                </li>
               ))}
             </ul>
           ) : (
             <p>No search results found.</p>
-          )
-        )}
-      </div>
+          )}
+        </div>
+      ) : null}
     </div>
   );
 }
