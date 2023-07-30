67a08578f4971f52882c56139358b7211a68869d
da077 <da077@DESKTOP-HV19KNK>
2023-07-30 17:53:41 +0300

    hh

--- src/js/favorite_playlist.js
+++ src/js/favorite_playlist.js
@@ -8,6 +8,7 @@ import {
 import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import "../css/favorite.css";
 
 function FavoritePlaylist() {
   const [playList, setPlayList] = useState([]);
