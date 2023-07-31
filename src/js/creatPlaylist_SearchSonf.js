b25c4345ed7308c3ecd368ccc3ce634a935a8ee5
HeroutRozilyo <heroot12@gmail.com>
2023-07-31 23:22:55 +0300

    search

--- src/js/creatPlaylist_SearchSonf.js
+++ src/js/creatPlaylist_SearchSonf.js
@@ -4,13 +4,14 @@ import { faPlus } from "@fortawesome/free-solid-svg-icons";
 
 function SearchSongCreat(props) {
   const [searchTerm, setSearchTerm] = useState("");
+  const [selectedSongs,setSelectedSongs]=useState([]);
   const [searchResults, setSearchResults] = useState([]);
   const [isSearching, setIsSearching] = useState(false);
   const [isInputFocused, setIsInputFocused] = useState(false);
-
+  const [isMouseOverResults, setIsMouseOverResults] = useState(false); // New state variable
   const CACHE_KEY = "songCache";
-   // Ref to the search container
-   
+ 
+
   useEffect(() => {
     // Check if the cache exists and is not empty
     const cachedData = JSON.parse(localStorage.getItem(CACHE_KEY));
@@ -30,7 +31,6 @@ function SearchSongCreat(props) {
             data: data,
           };
           localStorage.setItem(CACHE_KEY, JSON.stringify(cacheData));
-          setSearchResults(data.slice(0, 10)); // Show the first 10 songs in the results
         } catch (error) {
           console.error(error);
         }
@@ -39,31 +39,8 @@ function SearchSongCreat(props) {
       fetchSongsAndCache();
       const interval = setInterval(fetchSongsAndCache, 10 * 60 * 1000); // 10 minutes interval
       return () => clearInterval(interval);
-    } else {
-      setSearchResults(cachedData.data.slice(0, 10)); // Show the first 10 cached songs in the results
     }
-
-    // Set the initial focus state to false when the component mounts
-    setIsInputFocused(false);
   }, []);
-
-
-  
-  // useEffect(() => {
-  //   // Add event listener to the document for clicks outside the search component
-  //   // const handleOutsideClick = (event) => {
-  //   //   if (searchRef.current && !searchRef.current.contains(event.target)) {
-  //   //     setSearchResults([]); // Close the search results
-  //   //   }
-  //   // };
-
-  //   document.addEventListener("click", handleOutsideClick);
-
-  //   return () => {
-  //     // Remove the event listener when the component unmounts
-  //     document.removeEventListener("click", handleOutsideClick);
-  //   };
-  // }, []);
 
   useEffect(() => {
     handleSearch();
@@ -80,49 +57,63 @@ function SearchSongCreat(props) {
     }
   };
 
-  const handleSongSelect = (song) => { 
-    console.log("Selected Song:", song); // Add this line to check if the function is triggered and getting the song data.
-    console.log("Song Name:", song.SongName);
-    props.onSongSelect(song);
-  };
-
   
 
-  return (
-    <div className="search-songs"  >
-      <div className="search-input-container">
-        <input
-          type="text"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
-          placeholder="Search for songs..."
-          onFocus={() => {
-            setIsInputFocused(true);
-            if (!searchTerm) {
-              // Show all the cached songs if the search term is empty
-              const cachedData = JSON.parse(localStorage.getItem(CACHE_KEY));
-              if (cachedData && cachedData.data) {
-                setSearchResults(cachedData.data.slice(0, 10));
-              }
-            }
-          }}
-          onBlur={() => setIsInputFocused(false)} 
-        />
-        <button className="search-button" onClick={handleSearch}>
-          Search
-        </button>
-      </div>
-      <div
-        className="search-results"
-        // onMouseEnter={() => setIsMouseOverResults(true)}
-        // onMouseLeave={() => setIsMouseOverResults(false)}
-      >
+  const handleShowAllSongs = () => {
+    // Show all the song links when the user focuses on the input without typing anything
+    if (searchTerm.trim() === "") {
+      const cachedData = JSON.parse(localStorage.getItem(CACHE_KEY));
+      if (cachedData && cachedData.data) {
+        setSearchResults(cachedData.data);
+      }
+    }
+  };  
+  const handleSongSelect = (selectedSong) => {
+    const isSongSelected = selectedSongs.find(
+      (song) => song.SongID === selectedSong.SongID
+    );
+
+    // If the song is not already in the list, add it
+    if (!isSongSelected) {
+      setSelectedSongs([...selectedSongs, selectedSong]);
+    } else {
+      // If the song is already in the list, you can show an alert or handle it as per your requirements
+      alert("This song is already in the playlist!");
+    }
+  };
+
+// ... (previous code)
+
+return (
+  <div className="search-songs">
+    <div
+      className="search-input-container"
+      onMouseEnter={() => setIsMouseOverResults(true)}
+      onMouseLeave={() => setIsMouseOverResults(false)}
+    >
+      <input
+        type="text"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+        onFocus={() => {
+          setIsInputFocused(true);
+          handleShowAllSongs(); // Show all songs when the input is focused
+        }}
+        onBlur={() => setIsInputFocused(false)}
+        placeholder="Search for songs..."
+      />
+      <button className="search-button" onClick={handleSearch}>
+        Search
+      </button>
+    </div>
+    {isInputFocused || isMouseOverResults? (
+      <div className="search-results">
         {isSearching ? (
           <p>Loading...</p>
-        ) : (
+        ) : Array.isArray(searchResults) && searchResults.length > 0 ? (
           <ul>
             {searchResults.map((song) => (
-              <li key={song.SongID} onMouseDown={(event) => handleSongSelect(song, event)}>
+              <li key={song.SongID}>
                 <button onClick={() => handleSongSelect(song)}>
                   <FontAwesomeIcon icon={faPlus} />
                 </button>
@@ -130,13 +121,15 @@ function SearchSongCreat(props) {
               </li>
             ))}
           </ul>
-        )}
-        {searchResults.length === 0 && !isSearching && isInputFocused && (
+        ) : (
           <p className="no-results">No results found.</p>
         )}
       </div>
-    </div>
-  );
-}
+    ) : null}
+  </div>
+);
+
+        }
 
 export default SearchSongCreat;
+
